2e9e909286fd726db35139fbb2b8820f9d7eaf12
agustindelmonti <agus.delmonti@gmail.com>
2026-08-01 12:41:27 -0300

    Scaffold contracts/ Hardhat package for Avalanche Fuji integration

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,14 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.24',
+  networks: {
+    fuji: {
+      url: process.env.POLFIN_FUJI_RPC_URL || 'https://api.avax-test.network/ext/bc/C/rpc',
+      chainId: 43113,
+      accounts: process.env.POLFIN_OPERATOR_PRIVATE_KEY ? [process.env.POLFIN_OPERATOR_PRIVATE_KEY] : [],
+    },
+  },
+};
